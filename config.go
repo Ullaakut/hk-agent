b2046ea124bb332d9a6ddcd9d86e8959421e302b
Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>
2018-04-06 17:36:04 +0200

    Display metrics at every refresh and display alerts while traffic threshold is reached

--- config.go
+++ config.go
@@ -10,17 +10,25 @@ import (
 
 // Config represents the HKAgent configuration
 type Config struct {
-	LogLevel      string
-	LogFilePath   string
+	LogLevel string
+	// File path to the log file that will be read by hk-agent
+	LogFilePath string
+	// traffic threshold that triggers an alert when traffic from the last 2mns represents more
+	// megabytes than this number
+	TrafficThreshold uint64
+	// number of top hits to display when processing metrics
+	TopHitsNumber int
 	RefreshPeriod time.Duration
 }
 
 // DefaultConfig generates a configuration structure with the default values
 func DefaultConfig() Config {
 	return Config{
-		LogLevel:      "DEBUG",
-		LogFilePath:   "logs",
-		RefreshPeriod: 10 * time.Second,
+		LogLevel:         "DEBUG",
+		LogFilePath:      "logs",
+		TrafficThreshold: 4096,
+		TopHitsNumber:    3,
+		RefreshPeriod:    10 * time.Second,
 	}
 }
 
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 func main() {
+	// instantiate structured logger
 	log := NewZeroLog(os.Stderr)
 
 	config := DefaultConfig()
@@ -23,37 +24,57 @@ func main() {
 	sig := make(chan os.Signal)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 
-	file, err := os.Open(config.LogFilePath)
-	if err != nil {
-		log.Error().Err(err).Msg("Could not open logfile")
-	}
-	defer file.Close()
-
-	parser := gonx.NewParser(`$client_address $identifier $user_id [$time] "$request" $status $size`)
-
-	go func() {
-		for {
-			entries := []*HTTPEntry{}
-
-			scanner := bufio.NewScanner(file)
-			for scanner.Scan() {
-				entry, err := parser.ParseString(scanner.Text())
-				if err != nil {
-					log.Error().Err(err).Msg("Could not parse string")
-				} else {
-					entries = append(entries, NewHTTPEntry(log, entry))
-				}
-			}
-			// TODO: Display top hits here
-			// TODO: Check traffic on last two minutes
-			// TODO: Interface system that keeps alarm information visible at all times
-			time.Sleep(config.RefreshPeriod)
-		}
-	}()
+	// read logs in a separate routin
+	go readLogs(log, config)
 
 	// Wait for agent to be stopped
 	<-sig
 	signal.Stop(sig)
 	close(sig)
 	os.Exit(0)
+}
+
+// Reads the logs from the file specified in the configuration
+// and process the entries using the configured values
+func readLogs(log *zerolog.Logger, config Config) {
+	// instanciate parser for Common Log Format
+	parser := gonx.NewParser(`$client_address $identifier $user_id [$time] "$request" $status $size`)
+
+	// instantiate log processor
+	logProcessor := NewLogProcessor(log, config.TopHitsNumber, config.TrafficThreshold)
+
+	// open log file
+	file, err := os.Open(config.LogFilePath)
+	if err != nil {
+		log.Error().Err(err).Msg("Could not open logfile")
+	}
+	defer file.Close()
+
+	for {
+		timeEnd := time.Now().Add(config.RefreshPeriod)
+
+		// Recreate scanner at every iteration to ensure that it gets the new data
+		scanner := bufio.NewScanner(file)
+		entries := []*HTTPEntry{}
+		for scanner.Scan() {
+			// parse every line of the log file into an HTTP entry
+			line := scanner.Text()
+			if line != "" {
+				entry, err := parser.ParseString(line)
+				if err != nil {
+					log.Error().Err(err).Msg("Could not parse string")
+				} else {
+					// convert parsed entry into our own HTTPEntry strucutre
+					entries = append(entries, NewHTTPEntry(log, entry))
+				}
+			}
+		}
+
+		// add all parsed entries to logProcessor
+		go logProcessor.add(entries)
+
+		// Sleep for 10 seconds minus the time that this loop took to complete
+		// If this loop took more than 10s to complete, sleep will return immediately
+		time.Sleep(timeEnd.Sub(time.Now()))
+	}
 }
