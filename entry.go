817f292f06af4fd01f66da15e223f3d9ef728b4c
Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>
2018-04-06 17:36:04 +0200

    Metrics & Alerting (#2) (#3)

--- entry.go
+++ entry.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -19,9 +21,10 @@ type HTTPEntry struct {
 	StatusStr     string `json:"status"`
 	SizeStr       string `json:"size"`
 
-	Status uint64
-	Size   uint64
-	Time   time.Time
+	Section string
+	Status  uint64
+	Size    uint64
+	Time    time.Time
 }
 
 func (h *HTTPEntry) parseStrings(log *zerolog.Logger) {
@@ -40,6 +43,11 @@ func (h *HTTPEntry) parseStrings(log *zerolog.Logger) {
 	h.Size, err = strconv.ParseUint(h.SizeStr, 10, 64)
 	if err != nil {
 		log.Warn().Err(err).Msg("could not parse time")
+	}
+
+	h.Section, err = parseSection(log, h.Request)
+	if err != nil {
+		log.Warn().Err(err).Msg("could not parse section")
 	}
 }
 
@@ -63,10 +71,42 @@ func NewHTTPEntry(log *zerolog.Logger, entry *gonx.Entry) *HTTPEntry {
 		Str("identifier", httpEntry.Identifier).
 		Str("user_id", httpEntry.UserID).
 		Str("request", httpEntry.Request).
+		Str("section", httpEntry.Section).
 		Uint64("status", httpEntry.Status).
 		Uint64("size", httpEntry.Size).
 		Time("timestamp", httpEntry.Time).
 		Msg("Request received")
 
 	return httpEntry
+}
+
+func parseSection(log *zerolog.Logger, request string) (string, error) {
+	// if the request isn't provided in the logs, no section can be parsed
+	if request == "-" {
+		return "-", nil
+	}
+
+	// Find beginning of section string
+	sectionPos := strings.Index(request, " ") + 1
+	// Find end of section string
+	endOfSectionPos := strings.Index(request[sectionPos:], " ")
+	// Find subsection position if it exists
+	subsectionPos := strings.Index(request[sectionPos+1:], "/") + 1
+
+	// Request string does not contain the format "METHOD /route/ ..."
+	if sectionPos == -1 || endOfSectionPos == -1 {
+		return "", errors.New("invalid request format")
+	}
+
+	// Remove / from section name
+	if request[sectionPos+endOfSectionPos] == '/' {
+		endOfSectionPos++
+	}
+
+	// if there is no subsection, return the whole route
+	if endOfSectionPos < subsectionPos {
+		return request[sectionPos : endOfSectionPos+sectionPos], nil
+	}
+	// otherwise, return only the section
+	return request[sectionPos : subsectionPos+sectionPos], nil
 }
